9fe0a2c222a377043f6202155d2daf0632d6dca5
MrPiazzo <74234366+MrPiazzo@users.noreply.github.com>
2021-01-20 00:49:43 +0100

    Update chiptube.sh

--- chiptube.sh
+++ chiptube.sh
@@ -1,48 +1,47 @@
 #! /bin/bash
 
-## Help message: when no parameter is provided
+## A help message is provided when no parameters file is specified.
 if [ $# -ne 1 ]
 then
-  echo "The number of arguments is: $#"
-  echo "Usage: bash chipseq <params_file>"
-  echo ""
-  echo "params.file: Input file with the parameters"
-  echo "An example of params can be found in the test folder"
+  echo "The number of arguments provided is: $#"
+  echo "The number of arguments provided is wrong."
+  echo "Usage: bash chiptube.sh <params_file>"
+  echo "chiptube.sh requires a params_file with values for ALL the necessary parameters."
+  echo "An example of this can be found in the test folder."
+  echo "For more info, check README.md"
   exit
 fi
 
-
-##Reading in parameter file 
+## Reading in params_file.
+echo ""
+echo "======================"
+echo "| LOADING PARAMETERS |"
+echo "======================"
+echo ""
 
 PARAMS=$1
-
-echo ""
-echo "==================="
-echo "LOADING PARAMETERS:"
-echo "==================="
-echo ""
-
 INSDIR=$(grep installation_directory: $PARAMS | awk '{print($2)}')
-echo "Installation directory=$INSDIR"
+echo "Installation directory = $INSDIR"
 WD=$(grep working_directory: $PARAMS | awk '{print($2)}')
 echo "Working directory = $WD"
 EXP=$(grep experiment_name: $PARAMS | awk '{ print($2)}')
-echo "Experiment name= $EXP"
+echo "Experiment name = $EXP"
 NUMREPLICAS=$(grep number_replicas: $PARAMS | awk '{print($2)}')
 echo "Number of replicas = $NUMREPLICAS"
 GENOME=$(grep path_genome: $PARAMS | awk '{print($2)}')
 echo "Reference genome = $GENOME"
 ANNOTATION=$(grep path_annotation: $PARAMS | awk '{print($2)}')
-echo "Annotation= $ANNOTATION"
+echo "Annotation = $ANNOTATION"
 CHR=$(grep universe_chromosomes: $PARAMS | awk '{print($2)}')
-echo "Chromosomes for universe= $CHR"
+echo "Chromosomes for universe = $CHR"
 PVALUEGO=$(grep p_value_cutoff_go: $PARAMS | awk '{print($2)}')
-echo "P value cutoff for GO enrichment= $PVALUEGO"
+echo "P value cutoff for GO enrichment = $PVALUEGO"
 PVALUEKEGG=$(grep p_value_cutoff_kegg: $PARAMS | awk '{print($2)}')
-echo "P value cutoff for KEGG enrichment= $PVALUEKEGG"
+echo "P value cutoff for KEGG enrichment = $PVALUEKEGG"
 PEAK=$(grep type_of_peak: $PARAMS | awk '{print($2)}')
-echo "Type of peak= $PEAK"
+echo "Type of peak = $PEAK"
 
+## Creating arrays for ChIP and input samples and filling them with the previously specified files.
 CHIPS=()
 INPUTS=()
 i=0
@@ -53,29 +52,25 @@ do
         INPUTS[$i]=$(grep path_sample_input_$j: $PARAMS | awk '{print($2)}')
         ((i++))
 done
-
 echo "Samples ="
 echo "${CHIPS[@]}"
 echo "${INPUTS[@]}"
 
-
-##Generating work space
+## Generating work space.
 echo ""
-echo "====================="
-echo "GENERATING WORK SPACE"
-echo "====================="
+echo "========================="
+echo "| GENERATING WORK SPACE |"
+echo "========================="
 echo ""
 
 cd $WD
 mkdir $EXP
 cd $EXP
 mkdir genome annotation results samples
-
 cd genome
 cp $GENOME genome.fa
 cd ../annotation
 cp $ANNOTATION annotation.gtf
-
 cd ../samples
 i=1
 while [ $i -le $NUMREPLICAS ]
@@ -93,35 +88,28 @@ do
         ((i++))
 done
 
+## Generating reference genome index.
+echo ""
+echo "=================="
+echo "| CREATING INDEX |"
+echo "=================="
+echo ""
 
-##Generating reference genome index
 cd ../genome
 bowtie2-build genome.fa index
-
 echo "File size:" du -h *
 
-
-echo ""
-echo "============="
-echo "INDEX CREATED"
-echo "============="
-echo ""
-
-
-##Processing individual samples 
-
-cd ../results
-
-i=1
-while [ $i -le $NUMREPLICAS ]
-do
-        qsub -o replica_$i -N replica_$i $INSDIR/tarea/chipseq_bag2020/sample_processing $WD/$EXP/samples/replica_$i $i $EXP $NUMREPLICAS $GENOME $INSDIR $CHR $PVALUEGO $PVALUEKEGG $PEAK
-        ((i++))
-done
-
+## Processing individual samples. 
 echo ""
 echo "================================="
 echo "| PROCESSING INDIVIDUAL SAMPLES |"
 echo "================================="
 echo ""
 
+cd ../results
+i=1
+while [ $i -le $NUMREPLICAS ]
+do
+        qsub -o replica_$i -N replica_$i $INSDIR/tarea/chipseq_bag2020/sample_processing $WD/$EXP/samples/replica_$i $i $EXP $NUMREPLICAS $GENOME $INSDIR $CHR $PVALUEGO $PVALUEKEGG $PEAK
+        ((i++))
+done
